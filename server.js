da2f0f917070b97fc08bc19ff318486bff674315
lucawic <lucaswick@email.arizona.edu>
2021-08-29 12:33:42 -0700

    Adding HEROKOOOOO

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.use('/', express.static(path.join(_dirname, 'public')))
+
+require ("./routes/apiRoutes")(app);
+require ("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT" + PORT);
+});
